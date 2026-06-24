a3d74fc729038bccc0521f0863157aedc2d7f7f7
UjwalCode01 <ujwaldaivikar@gmail.com>
2026-06-24 10:07:18 +0530

    second program

--- lecture_two.py
+++ lecture_two.py
@@ -6,3 +6,24 @@ str = str.capitalize()
 print(str)
 print(str.replace("o","a"))#replaces all occurences of old value
 print(str.replace("python","java"))
+print(str.count("i"))
+
+#conditional statemnets
+#if-elif-else(syntax) #if is always check # condition depend # else are always in last 
+age = 23
+
+if(age >= 18):
+    print("can vote & apply for license")
+
+light = "black"
+
+if(light == "red"):
+    print("stop")
+elif(light == "green"):
+    print("go")
+elif(light == "yellow"):
+    print("wait")
+else:
+    print("light is broken")
+
+print("end of code")
