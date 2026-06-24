f2bb0faba7f04278e38f801201aff5397e52df2d
UjwalCode01 <ujwaldaivikar@gmail.com>
2026-06-24 09:48:10 +0530

    third program

---
+++ lecture_two.py
@@ -0,0 +1,8 @@
+print("hello world")
+str = "i am studying python"
+print(str.endswith("hon"))#returns true if string ends with substr
+print(str.capitalize())#capitalizes 1st char
+str = str.capitalize()
+print(str)
+print(str.replace("o","a"))#replaces all occurences of old value
+print(str.replace("python","java"))
