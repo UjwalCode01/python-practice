5e45464c59f7c817747730989c53e6fbd6ceb197
UjwalCode01 <ujwaldaivikar@gmail.com>
2026-06-24 09:37:37 +0530

    second program

--- firstprogram.py
+++ firstprogram.py
@@ -1,2 +1,27 @@
-print("hello world")
-print("my name is ujwal")
+#assignment operators 
+num = 10 
+num = num + 20 
+print(num)
+print("num :", num)
+
+#string
+str1 = "this is a string. \n we are creating it in python" #back (back slach)
+print(str1)
+
+#concentation
+str2 = "i am ujwal."
+str3 = "i live in yavatmal"
+final_str = str2 + str3
+print(final_str)
+#length of str
+len1 = str2
+print(len1)
+str = "ujwal" # index acess
+ch = str[0]
+print(ch)
+#slicing
+#accessing parts of a string 
+str4 = "i am ujwal"# positive index
+print(str[2:4])
+#negative index 
+print(str[-3:-1])
