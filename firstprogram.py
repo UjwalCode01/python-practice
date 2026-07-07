b490795c52d726cf275ebcfce44edaa6ac10b86c
UjwalCode01 <ujwaldaivikar@gmail.com>
2026-07-07 21:02:33 +0530

    lec 1

--- firstprogram.py
+++ firstprogram.py
@@ -1,27 +1,5 @@
-#assignment operators 
-num = 10 
-num = num + 20 
-print(num)
-print("num :", num)
-
-#string
-str1 = "this is a string. \n we are creating it in python" #back (back slach)
-print(str1)
-
-#concentation
-str2 = "i am ujwal."
-str3 = "i live in yavatmal"
-final_str = str2 + str3
-print(final_str)
-#length of str
-len1 = str2
-print(len1)
-str = "ujwal" # index acess
-ch = str[0]
-print(ch)
-#slicing
-#accessing parts of a string 
-str4 = "i am ujwal"# positive index
-print(str[2:4])
-#negative index 
-print(str[-3:-1])
+# lec 1 : varibles and data 
+print("hello world")
+print("i am ujwal vijay daivikar")
+print("i live in yavatmal")
+print("my age is 23","i am mechanical engineering")
