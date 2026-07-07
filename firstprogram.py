bab1ede8e6aa49129f4d739601a9d266f189834a
UjwalCode01 <ujwaldaivikar@gmail.com>
2026-07-07 21:10:30 +0530

    code change

--- firstprogram.py
+++ firstprogram.py
@@ -6,7 +6,13 @@ print("my age is 23","i am mechanical engineering")
 # varible :
 # jis ki value change ho rhi hai 
 name = "ujwal"#string means a double code 
-age = 23 
+age = 23 # "="assigement rule
 price = 23.98
+age2 = age
 # name is varible,ujwal in value
 print(name)
+print(age)
+print(price)
+print("my name is :",name)
+print("my age is :",age)
+print(age2)
