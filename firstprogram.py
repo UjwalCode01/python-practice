34be3659eb782eaafc8b61ecbfe35525e7f86a84
UjwalCode01 <ujwaldaivikar@gmail.com>
2026-07-07 21:05:51 +0530

    first program

--- firstprogram.py
+++ firstprogram.py
@@ -3,3 +3,7 @@ print("hello world")
 print("i am ujwal vijay daivikar")
 print("i live in yavatmal")
 print("my age is 23","i am mechanical engineering")
+# varible :
+# jis ki value change ho rhi hai 
+name = "ujwal"
+# name is varible,ujwal in value
